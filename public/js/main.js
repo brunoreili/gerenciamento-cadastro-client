0402dc5db083e2635b2e7d1c90fbb3bb8c0f0ac7
Bruno Bispo <brunoreili@hotmail.com>
2020-04-29 16:27:05 -0300

    feat/Implementação de rotas

--- public/js/main.js
+++ public/js/main.js
@@ -1,1 +1,20 @@
-angular.module('gerenciamentocadastro', []);
+angular.module('gerenciamentocadastro', ['ngRoute'])
+.config(function($routeProvider, $locationProvider) {
+
+    $locationProvider.html5Mode(true);
+
+    $routeProvider.when('/login', {
+        templateUrl: 'partials/login.html',
+    });
+
+    $routeProvider.when('/operadores', {
+        templateUrl: 'partials/operadores.html',
+        controller: 'OperadoresController'
+    });
+
+    $routeProvider.when('/operadores/new', {
+        templateUrl: 'partials/cadastroOperador.html',
+    });
+    
+    $routeProvider.otherwise({ redirectTo: '/login' });
+});
