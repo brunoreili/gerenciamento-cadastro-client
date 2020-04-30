35baba664dbe34e690d88ebcc481d1085219d3d6
Bruno Bispo <brunoreili@hotmail.com>
2020-04-30 10:19:49 -0300

    fix/Inclusão de cabeçalho no post para tentativa de retirada de CORS

--- public/js/controllers/cadastro-operador-controller.js
+++ public/js/controllers/cadastro-operador-controller.js
@@ -6,15 +6,25 @@ angular.module('gerenciamentocadastro').controller('CadastroOperadorController',
 
     $scope.submeter = function() {        
         if($scope.formulario.$valid) {            
-            const objOperador = this.formatarObjOperador();
-            $http.post('http://localhost:8080/resources/operadoress', objOperador)
+            var objOperador = this.formatarObjOperador();
+            // $http.post('http://localhost:8080/resources/operadores', objOperador)
+            $http({
+                method: 'POST',
+                url: 'http://localhost:8080/resources/operadores',
+                data: objOperador,
+                headers: {
+                    'Access-Control-Allow-Origin' : '*',
+                    'Access-Control-Allow-Methods' : 'GET, POST, PUT, DELETE, OPTIONS, HEAD',
+                    'Content-Type': 'application/json',
+                    'Accept': 'application/json'
+                },
+            })
             .success(function() {
                 $scope.operador = {};
                 $scope.formulario.$submitted = null;
                 $scope.mensagem = "Operador incluído com sucesso!";
             })  
             .error(function(erro) {
-                debugger;
                 console.log(erro);
                 $scope.countErros++;
                 $scope.mensagem = "Não foi possível incluir o operador!";
@@ -24,11 +34,11 @@ angular.module('gerenciamentocadastro').controller('CadastroOperadorController',
 
     $scope.formatarObjOperador = function() {
                
-        const objOperador = {
+        var objOperador = {
             nome: $scope.operador.nome,
             login: $scope.operador.login,
             senha: $scope.operador.senha,
-            perfil: $scope.operador.perfil
+            perfil: parseInt($scope.operador.perfil)
         };
 
         return objOperador;
