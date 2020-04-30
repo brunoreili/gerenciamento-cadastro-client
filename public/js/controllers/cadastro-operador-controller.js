6a4f822b04e01f1d5f2a53ffa581f5dcb2d2d4f2
Bruno Bispo <brunoreili@hotmail.com>
2020-04-29 21:49:17 -0300

    fix/melhorando validações adicionando mensagem de erro

--- public/js/controllers/cadastro-operador-controller.js
+++ public/js/controllers/cadastro-operador-controller.js
@@ -1,25 +1,30 @@
 angular.module('gerenciamentocadastro').controller('CadastroOperadorController', function($scope, $http) {
 
     $scope.operador = {};
+    $scope.mensagem = '';
+    $scope.countErros = 0;
 
-    $scope.submeter = function() {
-
-        debugger;
-        var objOperador = this.formatarObjOperador();
-        console.log($scope.operador);
-
-        $http.post('http://localhost:8080/resources/operadores', objOperador)
-        .success(function() {
-            alert("Operador cadastrado com sucesso!");
-        })  
-        .error(function(erro) {
-            console.log(erro);
-        });
+    $scope.submeter = function() {        
+        if($scope.formulario.$valid) {            
+            const objOperador = this.formatarObjOperador();
+            $http.post('http://localhost:8080/resources/operadoress', objOperador)
+            .success(function() {
+                $scope.operador = {};
+                $scope.formulario.$submitted = null;
+                $scope.mensagem = "Operador incluído com sucesso!";
+            })  
+            .error(function(erro) {
+                debugger;
+                console.log(erro);
+                $scope.countErros++;
+                $scope.mensagem = "Não foi possível incluir o operador!";
+            });
+        }
     };
 
     $scope.formatarObjOperador = function() {
                
-        var objOperador = {
+        const objOperador = {
             nome: $scope.operador.nome,
             login: $scope.operador.login,
             senha: $scope.operador.senha,
@@ -28,4 +33,9 @@ angular.module('gerenciamentocadastro').controller('CadastroOperadorController',
 
         return objOperador;
     };
+
+    $scope.fecharAlerta = function() {
+        $scope.mensagem = '';
+        $scope.countErros = 0;
+    }
 })
--- public/js/controllers/operadores-controller.js
+++ public/js/controllers/operadores-controller.js
@@ -1,12 +1,14 @@
 angular.module('gerenciamentocadastro').controller('OperadoresController', function($scope, $http) {
 
     $scope.operadores = [];
+    $scope.mensagem = '';
 
     $http.get('http://localhost:8080/resources/operadores')
     .success(function(operadores) {
         $scope.operadores = operadores;
     })
     .error(function(error) {
+        $scope.mensagem = 'Não foi possível listar os operadores!';
         console.log(error);
     });
 });
